2c55c758f4591e73530df5585c69536521c0a5a0
David-Nicholas <cocoreanudavid@yahoo.com>
2024-10-03 11:43:27 +0300

    Create pages and navbar

---
+++ components/CustomButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <button @click="handleClick">{{ buttonText }}</button>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      buttonText: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      handleClick() {
+        this.$emit('activate');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  button {
+    border: 2px solid #2596be;
+    background-color: #2596be;
+    color: white;
+    padding: 8px 16px;
+    cursor: pointer;
+    border-radius: 6px;
+    font-size: 16px;
+    margin: 8px;
+    font-weight: bold;  
+  }
+  
+  button:hover {
+    background-color: #005566;
+    border-color: #005566;
+  }
+  </style>
+  
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig:{
+    public:{
+      deployment_url: process.env.DEPLOYMENT_URL,
+
+      auth_region: process.env.AUTH_REGION,
+      auth_userpool_id: process.env.AUTH_USERPOOL_ID,
+      auth_userpool_clientid: process.env.AUTH_USERPOOL_CLIENTID,
+      auth_userpool_endpoint: process.env.AUTH_USERPOOL_ENDPOINT,
+
+      oauth_clientid: process.env.OAUTH_CLIENTID,
+      oauth_domain: process.env.OAUTH_DOMAIN,
+    }
+  }
 })
